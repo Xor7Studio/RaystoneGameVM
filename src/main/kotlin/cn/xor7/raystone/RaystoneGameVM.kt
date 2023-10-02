98fd412d1dc1502417834b6303675be581e9acdc
MC_XiaoHei <xiaohei.xor7studio@foxmail.com>
2023-10-02 16:43:24 +0800

    Chore: add some todo

--- src/main/kotlin/cn/xor7/raystone/RaystoneGameVM.kt
+++ src/main/kotlin/cn/xor7/raystone/RaystoneGameVM.kt
@@ -1,14 +1,15 @@
 package cn.xor7.raystone
 
-import cn.xor7.raystone.event.Listener
 import cn.xor7.raystone.event.Subscribe
 import cn.xor7.raystone.event.lifecycle.ChannelInitializeEvent
+import org.bukkit.Bukkit
 import org.bukkit.plugin.java.JavaPlugin
 
-class RaystoneGameVM : JavaPlugin(), Listener {
+class RaystoneGameVM : JavaPlugin(), cn.xor7.raystone.event.Listener {
     override fun onEnable() {
         Raystone.init()
         Raystone.registerListener(this)
+        Bukkit.getPluginManager().registerEvents(BukkitEventListener, this)
         logger.info("Raystone Game VM Enabled.")
     }
 
